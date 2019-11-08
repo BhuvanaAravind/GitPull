0f32f32ff1066a24275bee8b7c5ddb3ab27ecbdc
BhuvanaAravind <Bhuvanakrish55@gmail.com>
2019-11-08 16:35:14 +0530

    user stories getting added

---
+++ src/org/test/UserStories.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class UserStories {
+	private void git() {
+		 System.out.println("git");	
+
+	}
+	private void hub() {
+		System.out.println("hub");	
+
+	}
+	
+	private void github() {
+	System.out.println("github");	
+
+	}
+
+}
